1a22de0c052fa14e4accbf975abca140d8ae3bcf
Guitaramust <guitaramust@gmail.com>
2026-04-18 00:01:31 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  // Обязательно абсолютный путь к репозиторию
+  base: "/crypto-app/",
   plugins: [react()],
+  root: "../", // Указываем, что index.html находится на уровень выше
+  build: {
+    outDir: "dist", // Папка сборки будет в корне проекта
+    emptyOutDir: true,
+  },
 });
